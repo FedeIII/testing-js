49494635736472171093596162d89a0bf895ad93
FedeIII <fedelll@gmail.com>
2018-04-17 18:24:57 +0200

    added coverage, better test descriptions

--- src/utils/lesson1.test.js
+++ src/utils/lesson1.test.js
@@ -2,29 +2,29 @@ import { twoSum, twoSum2 } from './lesson1';
 
 describe('Lesson 1', () => {
   describe('twoSum', () => {
-    it('generic inputs', () => {
+    it('returns the positions of the elements that sum the target', () => {
       expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
     });
     
-    it('unordered elements', () => {
+    it('with unordered elements', () => {
       expect(twoSum([7, 11, 2, 15], 9)).toEqual([0, 2]);
     });
 
-    it('negative elements', () => {
+    it('with negative elements', () => {
       expect(twoSum([2, -3, 12, 15], 9)).toEqual([1, 2]);
     });
   });
 
   describe('twoSum2', () => {
-    it('generic inputs', () => {
+    it('returns the positions of the elements that sum the target', () => {
       expect(twoSum2([2, 7, 11, 15], 9)).toEqual([0, 1]);
     });
     
-    it('unordered elements', () => {
+    it('with unordered elements', () => {
       expect(twoSum2([7, 11, 2, 15], 9)).toEqual([0, 2]);
     });
 
-    it('negative elements', () => {
+    it('with negative elements', () => {
       expect(twoSum2([2, -3, 12, 15], 9)).toEqual([1, 2]);
     });
   });
