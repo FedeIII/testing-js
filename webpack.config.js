b674d1ccca030488e994ba813e75c591ab014103
FedeIII <fedelll@gmail.com>
2018-05-25 10:49:32 -0400

    added babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['babel-polyfill', './src/index.js'],
     output: {
         filename: './dist/bundle.js'
     },
